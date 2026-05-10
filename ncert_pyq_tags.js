3bb883300feb45295d31b7e09488223cc4269a2b
mNEET-7699 <monirulgen43@gmail.com>
2026-05-10 08:52:45 +0530

    Delete ncert_pyq_tags.js

--- ncert_pyq_tags.js
+++
@@ -1,9 +0,0 @@
-// Eita ekta utility file jekhane tumi year onujayi JSON data rakhte paro
-const pyqData = {
-    "Biology": [
-        {"word": "cell theory", "year": "2019", "quizId": "q101"},
-        {"word": "mitosis", "year": "2021", "quizId": "q105"}
-    ]
-};
-// Reader file-e eita import kore dynamic tags add kora jabe
-
